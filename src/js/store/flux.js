dc49574a40d5b1d576895b25f0657c8c23e054b7
RubenLopezM <rublopmag@hotmail.com>
2021-11-04 17:24:28 +0000

    Changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,26 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			baseURL: "https://www.swapi.tech/api/",
+			planets: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getPlanets: () => {
+				fetch(getStore().baseURL.concat("planets"))
+					.then(response => {
+						if (response.ok) {
+							return response.json();
+						}
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+						throw new Error("Fallo");
+					})
+					.then(responseAsJson => {
+						setStore({ planets: responseAsJson.results });
+						console.log(responseAsJson);
+					})
+					.catch(error => {
+						alert("Error");
+					});
 			}
 		}
 	};
